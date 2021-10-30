a05a1244a35e978bb0a475ac0f8799647b59b9d4
henriquehsb09 <henriquehsb09@hotmail.com>
2021-10-30 09:50:49 -0300

    Exercicios projeto empresa

---
+++ empresa/empresa/src/test/java/com/hsb/empresa/EmpresaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsb.empresa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpresaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
